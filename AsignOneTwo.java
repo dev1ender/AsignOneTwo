bfe0eebd7083f326879980eda5c0e621889f1afc
ITSOK\anonm <dev1ender@gmail.com>
2017-05-31 19:12:18 +0530

    Complete
    all Done

---
+++ AsignOneTwo.java
@@ -0,0 +1,22 @@
+
+
+public class AsignOneTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20,b=10;
+		b=a-- - --a;
+		System.out.println("b :"+b);
+		
+		int c=a--;
+		System.out.println("c :"+c);
+		
+		int d=a>>2;
+		System.out.println("d :"+d);
+		
+		int e=a&b;
+		System.out.println("e :"+e);
+
+	}
+
+}
